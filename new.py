d31b7f8dcacb5a9acd7f2b149cd51e8535ec3bac
Zhuoxii <101062035+Zhuoxii@users.noreply.github.com>
2022-05-26 20:45:04 +0800

    Update and rename assignment2 (1).py to new.py

--- new.py
+++ new.py
@@ -7,10 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1YJNRT2dwVl68QCqueI5OElPsBQ0KJ7pY
 """
 
-!pip install pyspark
-from google.colab import drive
-drive.mount('/content/drive')
-
 from pyspark.sql.functions import explode, col
 from pyspark.sql.functions import udf
 from pyspark.sql.functions import lit
@@ -27,31 +23,30 @@ spark = SparkSession \
     .appName("Comp5349 Assignment2") \
     .getOrCreate()
 
-train_data = '/content/drive/MyDrive/comp5349/test.json'
-df= spark.read.option('multiline', 'true').json(train_data)
 
-df.show()
 
-# # 抽样!
+spark.sparkContext.setLogLevel("ERROR")
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", help="the output path",
+                        default='a2_out')
+args = parser.parse_args()
+output_path = args.output
+
+    # s3://comp5349-2022/test.json
+    # s3://comp5349-2022/train_separate_questions.json
+    # s3://comp5349-2022/CUADv1.json
+data = "s3://comp5349-2022/train_separate_questions.json"
+
+
+df= spark.read.option('multiline', 'true').json(data)
 df =  df.select(explode("data.paragraphs").alias("data"))\
-         .select('data',explode("data.context").alias("context"))
-df = df.head(3)
-df = spark.createDataFrame(df)
-df.show()
-
-df = df.select('data', 'context', explode("data.qas").alias("qas"))\
-       .select('context', 'qas', explode("qas").alias("qas2"))\
-       .select('context', 'qas2', "qas2.question", "qas2.answers","qas2.is_impossible")
-
-# df =  df.select(explode("data.paragraphs").alias("data"))\
-#          .select('data',explode("data.context").alias("context"))\
-#          .select('data', 'context', explode("data.qas").alias("qas"))\
-#          .select('context', 'qas', explode("qas").alias("qas2"))\
-#          .select('context', 'qas2', "qas2.question", "qas2.answers","qas2.is_impossible").cache()
+         .select('data',explode("data.context").alias("context"))\
+         .select('data', 'context', explode("data.qas").alias("qas"))\
+         .select('context', 'qas', explode("qas").alias("qas2"))\
+         .select('context', 'qas2', "qas2.question", "qas2.answers","qas2.is_impossible").cache()
 
 df.show()
 
-df.count()
 
 # rdd = df.rdd.map(list)
 # rdd1
@@ -83,7 +78,6 @@ df_impossible_negative = df_no_answer.withColumn('list_context',udf1(df_no_answe
                        .withColumn('answer_end', lit(0))\
                        .select('context', 'source', 'question', 'answer_start', 'answer_end', 'type')
 
-df_impossible_negative.show()
 
 ## 筛选出没有impossible negative的sample
 df_no_answer = df.filter(df.is_impossible == True).select('context','question','answers' ,"is_impossible")
@@ -197,13 +191,11 @@ df_3 = df_positive.groupBy('question').agg(f.countDistinct('context')).withColum
 df_4 = df_1.join(df_3, 'question','inner')
 df_1 = df_4.withColumn('extract_length',f.round(f.col('question_count')/f.col('other_contract_count'),0).astype('int'))
 
-df_1.show()
-
 ## 把postive的question和impossible negative的question join
 df_2 =  df_1.join(df_impossible_negative, 'question', 'inner').orderBy( 'context','question','source')\
           .select('context', 'question','source', 'answer_start', 'answer_end','extract_length','type')
 
-df_2.show()
+
 
 window1 = Window.partitionBy("context").orderBy('context','question')
 df_3 = df_2.groupBy('context','question','extract_length').agg(f.collect_set('source').alias('source_list')).orderBy('context','question')
@@ -215,7 +207,6 @@ df_3 = df_3.withColumn('cusum_lag_extract_length', f.sum(f.col('lag_extract_leng
           .drop('lag_extract_length', 'cusum_lag_extract_length')\
           .select('context','question','source_list','extract_start','extract_length','seq_len')
 
-df_3.show()
 
 def new_extract(extract_start, extract_length, seq_len):
   if extract_start <= seq_len and extract_start + extract_length <= seq_len + 1:
@@ -239,14 +230,13 @@ udf4 = udf(new_extract, ArrayType(IntegerType()))
 df_4 = df_3.withColumn('extract_start',udf4(f.col('extract_start'), f.col('extract_length'),f.col('seq_len'))[0])\
            .withColumn('extract_length',udf4(f.col('extract_start'), f.col('extract_length'),f.col('seq_len'))[1])
 
-df_4.show()
 
 impossible_negative = df_4.withColumn('extract_source', f.slice("source_list",start=f.col('extract_start'), length=f.col('extract_length')))
 impossible_negative =  impossible_negative.withColumn('source', explode(f.col('extract_source')))\
                                   .withColumn('answer_start', lit(0))\
                                   .withColumn('answer_end', lit(0))\
                                   .select('source', 'question', 'answer_start', 'answer_end')
-impossible_negative.show()
+
 
 """## 平衡 possible negative and postive"""
 
@@ -270,7 +260,6 @@ possible_negative = possible_negative.withColumn('source', explode('extract_sour
                                   .withColumn('answer_start', lit(0))\
                                   .withColumn('answer_end', lit(0))\
                                   .select('source', 'question', 'answer_start', 'answer_end')
-possible_negative.show()
 
 print("successfully!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
@@ -288,156 +277,5 @@ output = json.dumps(result, indent = 2)
 with open('result.json','w') as f:
   json.dump(output, f)
 
-# testing_df = df
-# testing_df.show(1)
-
-# testing_df.printSchema()
-
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import *
-# from pyspark.sql import Window, Row
-# from pyspark.sql.types import IntegerType, StringType, FloatType
-
-# testing_data_df= testing_df.select((explode("data").alias('data')))
-# testing_paragraph_df = testing_data_df.select(explode("data.paragraphs").alias("paragraph"))
-
-# testing_paragraph_df.show(5)
-
-# paragraphs_context_qas_df = testing_paragraph_df.select(
-#     col("paragraph.context").alias("paragraph_context"),
-#     explode("paragraph.qas").alias('qas'),
-#     )
-
-# paragraphs_context_qas_df.show(5)
-
-# qas_answers_df = paragraphs_context_qas_df.select(
-#     col("paragraph_context"),
-#     col("qas.question").alias("qas_question"),
-#     col("qas.is_impossible").alias("qas_is_impossible"),
-#     explode_outer("qas.answers").alias('answers'),
-# )
-
-# qas_answers_df.show(5)
-
-# final_table_df = qas_answers_df.select(
-#     col("paragraph_context"),
-#     col("qas_question"),
-#     col("qas_is_impossible"),
-#     col("answers.answer_start").alias("answer_start"),
-#     col("answers.text").alias("answer_text"),
-# )
-
-# final_table_df.show(5)
-
-# possible_sample_df = final_table_df.where(
-#     col("qas_is_impossible") == False
-# )
-# possible_sample_df.show(5)
-
-# possible_sample_rdd = possible_sample_df.rdd
-# possible_sample_rdd.take(3)
-
-# def row_to_tuple(row):
-#   return tuple(row)
-# possible_sample_rdd_tuple = possible_sample_rdd.map(row_to_tuple)
-# possible_sample_rdd_tuple.take(3)
-
-# import random
-# def create_possible_sample(record):
-#   output_list = []
-#   poss_record = []
-#   neg_record = []
-#   subStrings = []
-#   window = 4096
-#   stride = 2048
-#   answer_start = record[3]
-#   answer_end = record[3] + len(record[4])
-#   length_record = len(record[0])
-#   for i in range(0, length_record, stride):
-#     if i+window <= length_record:
-#       temp = record[0][i:i+window]
-#     else:
-#       temp = record[0][i:]
-#     subStrings.append(temp)
-#   for j in range(len(subStrings)):
-#     if (j * stride <= answer_start) and (answer_start < j * stride + window) and (j * stride + window < answer_end):
-#       poss_record.append(Row(source=record[0][j*stride:j*stride+window], question=record[1], answer_start=answer_start - j * stride, answer_end=window))
-#     elif(j * stride <= answer_start) and (answer_start < j * stride + window) and (answer_end < j * stride + window):
-#       poss_record.append(Row(source=record[0][j*stride:j*stride+window], question=record[1], answer_start=answer_start - j * stride, answer_end=answer_end-j * stride))
-#     elif(answer_start <= j * stride) and (j * stride + window < answer_end):
-#       poss_record.append(Row(source=record[0][j*stride:j*stride+window], question=record[1], answer_start=0, answer_end=window))
-#     elif(j * stride <= answer_end) and (answer_end < j * stride + window) and (answer_start < j * stride):
-#       poss_record.append(Row(source=record[0][j*stride:j*stride+window], question=record[1], answer_start=0, answer_end=answer_end-j * stride))
-#     else:
-#       neg_record.append(Row(source=record[0][j*stride:j*stride+window], question=record[1], answer_start=0, answer_end=0))
-#   if (len(poss_record)<len(neg_record)):
-#     random.shuffle(neg_record)
-#     neg_record = neg_record[:len(poss_record)]
-#   poss_record.extend(neg_record)
-#   return poss_record
-
-# poss_rdd = possible_sample_rdd_tuple.flatMap(create_possible_sample).cache()
-# poss_rdd.take(3)
-
-# possible_contract = possible_sample_df.groupBy("paragraph_context").count().withColumnRenamed("count","positive_contract_num")
-# possible_contract.show(5)
-
-# poss_dfColumns = ["data_source","qas_question","answer_start","answer_end"]
-# poss_df = poss_rdd.toDF(poss_dfColumns).cache()
-# poss_df.show(3)
-
-# positive_simple_df = poss_df.where(col("answer_start") != '0')
-# positive_simple_num_df = positive_simple_df.where(col("answer_end") != '0').groupBy("qas_question").count().withColumnRenamed("count","positive_simple_num")
-# positive_simple_num_df.show(5)
-
-# count_table = final_table_df.where(
-#   col("qas_is_impossible") == True
-# ).join(positive_simple_num_df,"qas_question")
-# count_table = count_table.join(possible_contract,"paragraph_context").cache()
-# count_table.show(5)
-
-# count_table_rdd = count_table.rdd
-# count_table_rdd = count_table_rdd.map(row_to_tuple)
-# count_table_rdd.take(5)
-
-# def count_negtive_sample(record):
-#   output = []
-#   if record[5] % record[6] == 0:
-#     n = record[5] / record[6]
-#   elif (record[5] % record[6] != 0) and (float(record[5] / record[6])-int(record[5] / record[6]) >= 0.5):
-#     n = record[5]//record[6]+1
-#   elif (record[5] % record[6] != 0) and (record[5] / record[6]-record[5] // record[6] <= 0.5):
-#     n = record[5]//record[6]
-#   output.append((record[0],record[1],record[2],record[3],record[4],n))
-#   return output
-# count_netiv_num_rdd = count_table_rdd.flatMap(count_negtive_sample)
-# count_netiv_num_rdd.take(5)
-
-# def create_negative_sample(record):
-#   neg_record = []
-#   subStrings = []
-#   window = 4096
-#   stride = 2048
-#   num = int(record[5])
-#   length_record = len(record[0])
-#   for i in range(0, length_record, stride):
-#     if i+window <= length_record:
-#       temp = record[0][i:i+window]
-#     else:
-#       temp = record[0][i:]
-#     subStrings.append(temp)
-#   for j in range(len(subStrings)):
-#       neg_record.append(Row(source=record[0][j*stride:j*stride+window], question=record[1], answer_start=0, answer_end=0))
-#   random.shuffle(neg_record)
-#   neg_record = neg_record[:num]
-#   return neg_record
-# imposs_rdd = count_netiv_num_rdd.flatMap(create_negative_sample).cache()
-# imposs_rdd.take(3)
-
-# output_rdd = poss_rdd.union(imposs_rdd)
-# output_rdd.take(5)
-
-# output_rddColumns = ["source","question","answer_start","answer_end"]
-# output_df = output_rdd.toDF(output_rddColumns).cache()
-# output_df.show()
-
+spark.stop()
+spark.stop()
