3b9c035171308672c61c1ea5a7695c2c821de595
Zhuoxii <101062035+Zhuoxii@users.noreply.github.com>
2022-05-27 16:36:05 +0800

    Update new.py

--- new.py
+++ new.py
@@ -16,7 +16,12 @@ from pyspark.sql import SparkSession
 spark = SparkSession \
     .builder \
     .appName("Comp5349 Assignment2") \
+    .config("spark.executor.memory", "4g")\
+    .config("spark.driver.memory", "6g") \
+    .config("spark.executor.cores", "4") \
+    .config("spark.Num-executors", "10") \
     .getOrCreate()
+
 
 
 
